b63d1bbd10818c983f5b08c27e43e2b8bf79cf8f
Yujin Hong <hhhyj1077@naver.com>
2022-05-15 23:28:45 +0900

    commit

--- baekjoon/src/week1/피보나치수10870.java
+++ baekjoon/src/week1/피보나치수10870.java
@@ -3,29 +3,34 @@ package week1;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class 피보나치수10870 {
 	static int i = 2;
+	
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String nString = br.readLine();
 		int n = Integer.parseInt(nString);
 		
-		int result = makeResult(n);
+		ArrayList<Integer> list = new ArrayList<>();
+		list.add(0);
+		list.add(1);
+		int result = makeResult(n, list);
 		System.out.println(result);
 	}
 
-	private static int makeResult(int n) {
-		int[] arr = new int[n];
-		arr[0] = 0;
-		arr[1] = 1;
-		if(arr[i]==0) {
-			arr[i] = arr[i-1]+arr[i-2];
+	private static int makeResult(int n, ArrayList<Integer> list) {
+		if(i<=n) {
+			list.add(list.get(i-1)+list.get(i-2));
 			i++;
- 		}
-		if(i==n) return arr[n];
-		else return makeResult(n);
+			return makeResult(n, list);
+		}
+		else {
+			return list.get(n);
+		}
+		
 	}
 
 }
